5cc6c5ad68eeaeb87f70872f48061c4e6c17f19f
Matthew Titmus <matthew.titmus@gmail.com>
2020-09-20 11:19:18 -0400

    Added new.sh script to pull previous notes

---
+++ new.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+
+
+NEW_SYNTAX="./new.sh YYYY.MM.DD <pastebin-url> <image-url>"
+
+if (( $# == 3 )); then
+    NEW_DATE=$1
+    NEW_PASTEBIN_URL=$2
+    NEW_IMAGE_URL=$3
+else
+    echo "syntax: ${NEW_SYNTAX}"
+    exit 1
+fi
+
+# Validate date input
+#
+if NEW_HR_DATE=$(date -j -f "%Y.%m.%d" "${NEW_DATE}" +'%d %B %Y' 2> /dev/null); then
+    echo "Got date: ${NEW_HR_DATE}"
+else
+    echo "syntax: ${NEW_SYNTAX}"
+    exit 1
+fi
+
+# Get image
+#
+if ! wget -O "/tmp/${NEW_DATE}.png" "${NEW_IMAGE_URL}" 2> /dev/null; then
+    echo "Error downloading raw Pastebin contents"
+    exit 1
+fi
+
+# Get Pastebin data
+#
+echo "Got Pastebin path: ${NEW_PASTEBIN_URL}"
+
+NEW_RAW_PASTEBIN_URL="https://pastebin.com/raw/"$(echo "${NEW_PASTEBIN_URL}" | sed 's|.*/||')
+
+echo "Raw Pastebin URL: ${NEW_RAW_PASTEBIN_URL}"
+
+if ! wget -O "/tmp/${NEW_DATE}.md" "${NEW_RAW_PASTEBIN_URL}" 2> /dev/null; then
+    echo "Error downloading raw Pastebin contents"
+    exit 1
+fi
+
+# If we got here, we have all the parts
+# Do the thing
+
+echo -e '![Our Board](images/'"${NEW_DATE}"'.png)\n' > "${NEW_DATE}.md"
+
+cat "/tmp/${NEW_DATE}.md" >> "${NEW_DATE}.md"
+rm "/tmp/${NEW_DATE}.md"
+
+mv "/tmp/${NEW_DATE}.png" images/
+
+echo "* [${NEW_HR_DATE}](${NEW_DATE}.md)" >> README.md
+
+echo "Done!"
